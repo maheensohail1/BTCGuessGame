de23a25fcb7b3657b557624c3fa2b91ba8e58602
maheensohail1 <maheensohail2003@gmail.com>
2025-04-07 14:01:16 +0500

    environment files updated

--- frontend/src/screens/GuessingPage.tsx
+++ frontend/src/screens/GuessingPage.tsx
@@ -2,7 +2,7 @@ import { useState, useEffect } from "react";
 import axios from "axios";
 import { useNavigate } from "react-router-dom";
 
-const API_URL = ""; // Updated when deploying
+const API_URL = import.meta.env.VITE_APP_API_URL;
 
 /**
  * Props passed to GuessingPage
